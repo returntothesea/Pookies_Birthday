4300d4028d53cca1218a5e94720e1a8fbcb7cc65
Scotty <throwitinthecoffee@gmail.com>
2017-06-14 17:15:56 -0700

    Fix performance issue on mobile devices.
    Phaser.AUTO was defaulting to WebGL, which was running slowly on mobile for some reason. Forcing it to run on Canvas using Phaser.CANVAS fixed this issue.

--- js/intro.js
+++ js/intro.js
@@ -18,7 +18,6 @@ Intro.prototype = {
 
 	addText: function() {
 		var style = { font: "35px Press Start 2P", fill: "#eee", align: "center" };
-		var style2 = { font: "40px Arial", fill: "#eee", align: "center" };
 		text = this.game.add.text(this.game.world.centerX, this.game.world.centerY / 1.5, "Pookie's\nBirthday\nAdventure", style);
 		text.anchor.setTo(0.5);
 	}
